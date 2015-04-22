27c80135157c5d27a4c831a9a8ce65922f353a2b
Lewis Huang <huangyun_122@hotmail.com>
2015-04-22 12:39:50 +0800

    sqltool
    tools used to perform sql managements

---
+++ SqlTool/SqlTool/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace SqlTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgvServerProperties = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvServerProperties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Connection String";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "LinkToDB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvServerProperties
+            // 
+            this.dgvServerProperties.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvServerProperties.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvServerProperties.Location = new System.Drawing.Point(12, 100);
+            this.dgvServerProperties.Name = "dgvServerProperties";
+            this.dgvServerProperties.RowTemplate.Height = 23;
+            this.dgvServerProperties.Size = new System.Drawing.Size(260, 150);
+            this.dgvServerProperties.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.dgvServerProperties);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvServerProperties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvServerProperties;
+    }
+}
+
---
+++ SqlTool/SqlTool/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.SqlServer.Management.Smo;
+using System.Threading.Tasks;
+
+namespace SqlTool
+{
+    
+    public partial class Form1 : Form
+    {
+
+        string instanceconnectionstring;
+        public Server dbserver;
+        public DataSet localset;
+        public DataTable tblServerProperties;
+        public Form1()
+        {
+            InitializeComponent();
+            localset = new DataSet("localset");
+            tblServerProperties = localset.Tables.Add("ServerProperties");
+            tblServerProperties.Columns.Add("ServerPropertyName");
+            tblServerProperties.Columns["ServerPropertyName"].DataType = typeof(string);
+            tblServerProperties.Columns.Add("ServerPropertyValue");
+            tblServerProperties.Columns["ServerPropertyValue"].DataType = typeof(string);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            instanceconnectionstring = textBox1.Text;
+             dbserver = new Server(instanceconnectionstring);
+            
+            
+            
+            dgvServerProperties.DataSource = localset;
+            dgvServerProperties.DataMember = localset.Tables["ServerProperties"].ToString();
+          
+            Task search = new Task( searchDBProperties);
+            search.Start();
+
+            
+           
+
+        }
+
+        public void searchDBProperties()
+        {
+            foreach (Database mydb in dbserver.Databases)
+            {
+                tblServerProperties.Rows.Add(mydb.Name.ToString());
+                foreach (Property dbpro in mydb.Properties)
+                {
+                    tblServerProperties.Rows.Add(dbpro.Name.ToString(),dbpro.Value.ToString());
+                }
+            }
+        }
+            
+    }
+}
